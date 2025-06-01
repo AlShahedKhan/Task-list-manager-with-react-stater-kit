7e3dcc7aa15a814e173ce7c0bd65a94b84d34af9
Abdullah Al Shahed <abdullahalshahedkhan@gmail.com>
2025-06-01 08:16:01 +0600

    create task and tasklist model and set there all relation

---
+++ app/Models/Task.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\TaskList;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Task extends Model
+{
+    protected $table = 'tasks';
+
+    protected $fillable = [
+        'title',
+        'description',
+        'is_completed',
+        'due_date',
+        'list_id'
+    ];
+
+    public function list(): BelongsTo
+    {
+        return $this->belongsTo(TaskList::class, 'list_id');
+    }
+}
